55337b5c712d88a8726cfa61cac18181c93fded9
pabcrudel <pabcrudel@gmail.com>
2023-09-25 18:49:59 +0200

    Creado un procedimiento que saluda

---
+++ t3/ejer-03-procedimiento.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Procedimiento</title>
+</head>
+<body>
+    <?php
+        function saludo () {
+            echo "Hello world";
+        }
+
+        saludo();
+    ?>
+</body>
+</html>
